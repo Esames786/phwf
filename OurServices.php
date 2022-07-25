b3927204c77dddd1e136b6f55051eba4c74347de
shariq <syedshariqali302@gmail.com>
2022-07-25 20:56:28 +0500

    changes

---
+++ OurServices.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Public Help Welfare || Services</title><!-- mobile responsive meta -->
+
+    <!-- mobile responsive meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" href="images/favicons/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="images/favicons/favicon-16x16.png" sizes="16x16">
+
+ 
+
+</head>
+<body>
+
+<div class="boxed_wrapper">
+
+
+<?php include './includes/header.php' ?>
+ 
+
+<div class="inner-banner has-base-color-overlay text-center" style="background: url(images/background/4.jpg);">
+    <div class="container">
+        <div class="box">
+            <h1>Our Services</h1>
+        </div>
+    </div>
+</div>
+<div class="breadcumb-wrapper">
+    <div class="container">
+        <div class="pull-left">
+            <ul class="list-inline link-list">
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="cause-1.html">Contributions</a>
+                </li>
+
+                <li>
+                    Services
+                </li>
+            </ul>
+        </div>
+        <div class="pull-right">
+            <!-- <a href="#" class="get-qoute"><i class="fa fa-arrow-circle-right"></i>Become a Member</a> -->
+        </div>
+    </div>
+</div>
+
+
+<section class="urgent-cause2 sec-padd">
+    <div class="container">
+
+        <div class="row">
+        <div class="section-title">
+                        <h2>OUR <span class="thm-color">SERVICES </span></h2>
+                    </div>
+            <article class="item col-md-6 col-sm- col-xs-12">
+                <figure class="img-box">
+                    <img style="width:100%;height:40rem" src="images/cause/1.jpg" alt="">
+                    <div class="overlay"><div class="inner-box"><div class="content-box"><button class="thm-btn style-2 donate-box-btn">donate now</button></div></div></div>
+                </figure>
+                
+                <div class="content">
+                    
+                    <div class="text center">
+                        <a href="#"><h4 class="title">Animals</h4></a>
+                        <p>We all share ownership of the world weather we are human and non-human (animals). When we begin to perceive the universe as a collective society, the world will begin to improve for everyone. The goal of PHW foundation is to demonstrate the trust form of empathy—doing for those who can never do anything for you in return. To assist those in whose shoes you will never walk. And to look after anyone and everyone who needs assistance, no matter who they are. Our goal is to rescue and rehabilitate one of our country's most neglected animals: abused and injured homeless, street animals, while also highlighting and assisting in the hardships of other marginalized groups and pressing societal issues that are often overlooked. We also love to make an animal houses in which animals are kept for the purpose of treatment because there are less veterinary clinic in which treatment can be carried out. For this we will have the team of caring nurses and good veterinary doctors those who will treat the animals with professional skills. </p>
+                    </div>
+                    
+                    
+                </div>                        
+                        
+            </article>
+            <article class="item col-md-6 col-sm- col-xs-12">
+                <figure class="img-box">
+                    <img style="width:100%;height:40rem" src="images/cause/2.jpg" alt="">
+                    <div class="overlay"><div class="inner-box"><div class="content-box"><button class="thm-btn style-2 donate-box-btn">donate now</button></div></div></div>
+                </figure>
+                
+                <div class="content">
+                    
+                    <div class="text center">
+                        <a href="#"><h4 class="title">Medical Care</h4></a>
+                        <p>According to World Health Organization, Pakistan stands on the position of 154th out of 195 countries. This situation is alarming. Pakistan only spends 1.2% of Gross Domestic Product on Health .This data explains Pakistani population spends more on their health treatment because of huge fee of Private hospitals and clinics. </p>
+                        <br>
+                        <p>Our vision is that there should be proper health care available and accessible for everyone in our society. The goal of PHW is to concentrate on the needy people in our surroundings by the human power and sincere donations. We want to help those who cannot afford the costly treatment. The PHW also provides the facility to arrange blood for the patients. Most of cancer patients are unaware of their disease; they may require repeated hospitalization, costly medications, and dialysis, putting a significant financial strain on their families and health-care systems. In so many cases, deaths occur within 1-3 years because they have late access to testing and treatment. Thus, the goal of PHW is to invest in community change and help reduce suffering.</p>
+                    </div>
+                   
+                    
+                </div>                        
+                        
+            </article>
+            <article class="item col-md-6 col-sm- col-xs-12">
+                <figure class="img-box">
+                    <img style="width:100%;height:40rem" src="images/cause/3.jpg" alt="">
+                    <div class="overlay"><div class="inner-box"><div class="content-box"><button class="thm-btn style-2 donate-box-btn">donate now</button></div></div></div>
+                </figure>
+                
+                <div class="content">
+                    
+                    <div class="text center">
+                        <a href="#"><h4 class="title">Education</h4></a>
+                        <p>The current scenario is this that Pakistan has just 62.3%  literacy rate which is increasing year after year but there is so much work to do in this sector. Nearly 11 million children are out of schools in Pakistan that shows we have a lot of work to perform for our future generation. Pakistan only spends 1.5% of Total GDP on Education that is so much low. We have to balance it for good results.</p>
+                        <p> Our aim is to provide resources for education to the people who can’t afford to take their child to the schools and high schools. Schooling is both the means and the end goal of a better life; the means because it enables an individual to earn a living and the end goal because it raises one's awareness on a variety of issues ranging from healthcare to relevant social behavior to recognizing one's rights, enabling one to evolve as a better citizen. PHW foundation gives special attention towards girl’s education so that girls with their families get empowered. </p>
+                    </div>
+                   
+                    
+                </div>                        
+                        
+            </article>
+            <article class="item col-md-6 col-sm- col-xs-12">
+                <figure class="img-box">
+                    <img style="width:100%;height:40rem" src="images/cause/4.jpg" alt="">
+                    <div class="overlay"><div class="inner-box"><div class="content-box"><button class="thm-btn style-2 donate-box-btn">donate now</button></div></div></div>
+                </figure>
+                
+                <div class="content">
+                    
+                    <div class="text center">
+                        <a href="#"><h4 class="title">Food</h4></a>
+                        <p>Pakistan is the developing countries and also under high debt. Pakistan ranks 88th in the Hunger index out of 107 countries.  In Pakistan 20 percent people are under nourished.  But in this sector there are number of organization like Edhi, Chipa and JDC are working on very large scale. PHW will also follow footprints of these organizations for their cause. </p>
+                        <p>Our aim is to provide food to those who are deprived of it. Access to a consistent, cheap, and nutritionally supply of food in a dignified manner is the first challenge to a human's class and integrity, and a lack of food is the first obstacle to reaching one's full potential. If one's stomach is empty, one does not have the luxury of developing. Join your hands with PHW foundation to feed empty stomach.</p>
+                    </div>
+                 
+                    
+                </div>                        
+                        
+            </article>
+        
+        </div>
+        <ul class="page_pagination center">
+            <li><a href="#" class="tran3s"><i class="fa fa-angle-left" aria-hidden="true"></i></a></li>
+            <li><a href="#" class="active tran3s">1</a></li>
+            <li><a href="#" class="tran3s"><i class="fa fa-angle-right" aria-hidden="true"></i></a></li>
+        </ul>
+    </div>
+</section>
+
+
+<?php include './includes/footer.php' ?>
+
+                                                                                             
+    <!-- Scroll Top  -->
+    <button class="scroll-top tran3s color2_bg"><span class="fa fa-angle-up"></span></button>
+    <!-- preloader  -->
+    <div class="preloader"></div>
+   
+    <?php include './donate.php' ?>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menu.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.mixitup.min.js"></script>
+    <script src="js/jquery.fancybox.pack.js"></script>
+    <script src="js/imagezoom.js"></script> 
+    <script src="js/jquery.magnific-popup.min.js"></script> 
+    <script src="js/jquery.polyglot.language.switcher.js"></script>
+    <script src="js/SmoothScroll.js"></script>
+    <script src="js/jquery.appear.js"></script>
+    <script src="js/jquery.countTo.js"></script>
+    <script src="js/validation.js"></script> 
+    <script src="js/wow.js"></script> 
+    <script src="js/jquery.fitvids.js"></script> 
+    <script src="js/nouislider.js"></script> 
+    <script src="js/isotope.js"></script> 
+    <script src="js/jquery.bootstrap-touchspin.js"></script> 
+    <script src="js/jquery-ui.js"></script>
+    <script src="js/bootstrap-select.min.js"></script>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+
+
+
+    <script src="js/custom.js"></script>
+
+</div>
+    
+</body>
+</html>
