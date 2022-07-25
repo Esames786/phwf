6b1697ebbac4b46a5344cf6053422794d5906060
shariq <syedshariqali302@gmail.com>
2022-07-25 20:41:31 +0500

    changes

--- ourProjects.php
+++ ourProjects.php
@@ -30,7 +30,7 @@
 <div class="inner-banner has-base-color-overlay text-center" style="background: url(images/background/4.jpg);">
     <div class="container">
         <div class="box">
-            <h1>Causes grid view</h1>
+            <h1>Our Projects</h1>
         </div>
     </div>
 </div>
@@ -42,16 +42,16 @@
                     <a href="index.php">Home</a>
                 </li>
                 <li>
-                    <a href="cause-1.html">Causes</a>
+                    <a href="cause-1.html">Contributions</a>
                 </li>
 
                 <li>
-                    Causes grid View
+                    Projects
                 </li>
             </ul>
         </div>
         <div class="pull-right">
-            <a href="#" class="get-qoute"><i class="fa fa-arrow-circle-right"></i>Become a Volunteer</a>
+            <a href="#" class="get-qoute"><i class="fa fa-arrow-circle-right"></i>Become a Member</a>
         </div>
     </div>
 </div>
@@ -70,18 +70,10 @@
                 <div class="content">
                     
                     <div class="text center">
-                        <a href="#"><h4 class="title">Childrens to get their home</h4></a>
-                        <p>Fusce et augue placerat, dictu velit sit amet, egestasuna. cras aliquam pretium ornar liquam metus. Aenean venenatis sodales...</p>               
+                        <a href="#"><h4 class="title">AN AMBULANCE SERVICE</h4></a>
+                        <p>In the start we have an ambulance with basic needs for the treatment of patients.  This will be increased time to time. This ambulance system is controlled by the organization itself and organization fully owned this asset. This is the basic need of any hospital setup. When there is an emergency in any case we use this ambulance for rescuing the patient for the required hospitals and organizations.</p>               
                     </div>
-                    <div class="progress-box">
-                        <div class="bar">
-                            <div class="bar-inner animated-bar" data-percent="48%"><div class="count-text">48%</div></div>
-                        </div>
-                    </div>
-                    <div class="donate clearfix">
-                        <div class="donate float_left"><span>Goal: $54000 </span></div>
-                        <div class="donate float_right">Raised: $24000</div>
-                    </div>
+                    
                     
                 </div>                        
                         
@@ -95,18 +87,10 @@
                 <div class="content">
                     
                     <div class="text center">
-                        <a href="#"><h4 class="title">We encourage girls education</h4></a>
-                        <p>Phasellus cursus nunc arcu, eget sollicitudin milacinia tempurs. Donec ligula turpis, egestas  at volutpat no liquam...</p>               
+                        <a href="#"><h4 class="title">WATER HAND PUMPS</h4></a>
+                        <p>In internal Sindh We started number of projects in which this projects is widely spread. The need of this project was the shortage of water in internal Sindh. This step was taken on behalf of letter of the person residing the same location. He expressed the need of water tankers and water hand pumps in the village. So we decided to move further step and did our best. We provided many water hand pumps and controlled the shortage of water. We further believe that this step which we took to overcome the shortage will continue and more work will be performed in this sector.</p>
                     </div>
-                    <div class="progress-box">
-                        <div class="bar">
-                            <div class="bar-inner animated-bar" data-percent="75%"><div class="count-text">75%</div></div>
-                        </div>
-                    </div>
-                    <div class="donate clearfix">
-                        <div class="donate float_left"><span>Goal: $92000 </span></div>
-                        <div class="donate float_right">Raised: $69000</div>
-                    </div>
+                   
                     
                 </div>                        
                         
@@ -120,18 +104,10 @@
                 <div class="content">
                     
                     <div class="text center">
-                        <a href="#"><h4 class="title">The people to help themselves</h4></a>
-                        <p>Etiam vitae leo diam pellentesque portaed eleifend ultricies risu, vel rutrum era commodos  Praesent finib congue euismod...</p>               
+                        <a href="#"><h4 class="title">An Aftar Drive</h4></a>
+                        <p>In the holy month of Ramadan we are always blessed by Almighty Allah. We as an organization did the Aftrari Set Up for the whole month of Ramadan. By this act we got so much happened and our heart was full of joy.</p>
                     </div>
-                    <div class="progress-box">
-                        <div class="bar">
-                            <div class="bar-inner animated-bar" data-percent="68%"><div class="count-text">68%</div></div>
-                        </div>
-                    </div>
-                    <div class="donate clearfix">
-                        <div class="donate float_left"><span>Goal: $78000 </span></div>
-                        <div class="donate float_right">Raised: $49000</div>
-                    </div>
+                   
                     
                 </div>                        
                         
@@ -145,18 +121,12 @@
                 <div class="content">
                     
                     <div class="text center">
-                        <a href="#"><h4 class="title">Childrens to get their home</h4></a>
-                        <p>Fusce et augue placerat, dictu velit sit amet, egestasuna. cras aliquam pretium ornar liquam metus. Aenean venenatis sodales...</p>               
+                        <a href="#"><h4 class="title">A HOSPITAL SETUP</h4></a>
+                        <p>For the purpose of medical treatments we are taking step ahead and creating a hospital setup in which the needy patients will be given treatment free of cost. We are giving quality treatment and the medicines by which they can heal themselves.
+Having this type of services enhances our skills and we do more and more for the people of our society who are deprived of health facilities. Further we can also create more setups So that we can overcome the toughest medical problems like cancer and many dangerous diseases.
+</p>
                     </div>
-                    <div class="progress-box">
-                        <div class="bar">
-                            <div class="bar-inner animated-bar" data-percent="48%"><div class="count-text">48%</div></div>
-                        </div>
-                    </div>
-                    <div class="donate clearfix">
-                        <div class="donate float_left"><span>Goal: $54000 </span></div>
-                        <div class="donate float_right">Raised: $24000</div>
-                    </div>
+                 
                     
                 </div>                        
                         
@@ -170,123 +140,12 @@
                 <div class="content">
                     
                     <div class="text center">
-                        <a href="#"><h4 class="title">The people to help themselves</h4></a>
-                        <p>Etiam vitae leo diam pellentesque portaed eleifend ultricies risu, vel rutrum era commodos  Praesent finib congue euismod...</p>               
+                        <a href="#"><h4 class="title">RASHAN DISTRIBUTION</h4></a>
+                        <p>The basic need of the people is their daily household things. Those who cannot afford like widows, orphans and less salaried families are given the RASHAN package at their door step. This step was taken due to recent hikes in inflation of country in which middle class families are also troubling.
+We need to take care of the basic facilities of poor families So that they and their children can eat full of stomach. If they will eat full then they can perform well.</p>               
                     </div>
-                    <div class="progress-box">
-                        <div class="bar">
-                            <div class="bar-inner animated-bar" data-percent="68%"><div class="count-text">68%</div></div>
-                        </div>
-                    </div>
-                    <div class="donate clearfix">
-                        <div class="donate float_left"><span>Goal: $78000 </span></div>
-                        <div class="donate float_right">Raised: $49000</div>
-                    </div>
-                    
-                </div>                        
-                        
+                </div>                          
             </article>
-            <article class="item col-md-4 col-sm-6 col-xs-12">
-                <figure class="img-box">
-                    <img src="images/cause/6.jpg" alt="">
-                    <div class="overlay"><div class="inner-box"><div class="content-box"><button class="thm-btn style-2 donate-box-btn">donate now</button></div></div></div>
-                </figure>
-                
-                <div class="content">
-                    
-                    <div class="text center">
-                        <a href="#"><h4 class="title">We encourage girls education</h4></a>
-                        <p>Phasellus cursus nunc arcu, eget sollicitudin milacinia tempurs. Donec ligula turpis, egestas  at volutpat no liquam...</p>               
-                    </div>
-                    <div class="progress-box">
-                        <div class="bar">
-                            <div class="bar-inner animated-bar" data-percent="75%"><div class="count-text">75%</div></div>
-                        </div>
-                    </div>
-                    <div class="donate clearfix">
-                        <div class="donate float_left"><span>Goal: $92000 </span></div>
-                        <div class="donate float_right">Raised: $69000</div>
-                    </div>
-                    
-                </div>                        
-                        
-            </article>
-            <article class="item col-md-4 col-sm-6 col-xs-12">
-                <figure class="img-box">
-                    <img src="images/cause/7.jpg" alt="">
-                    <div class="overlay"><div class="inner-box"><div class="content-box"><button class="thm-btn style-2 donate-box-btn">donate now</button></div></div></div>
-                </figure>
-                
-                <div class="content">
-                    
-                    <div class="text center">
-                        <a href="#"><h4 class="title">The people to help themselves</h4></a>
-                        <p>Etiam vitae leo diam pellentesque portaed eleifend ultricies risu, vel rutrum era commodos  Praesent finib congue euismod...</p>               
-                    </div>
-                    <div class="progress-box">
-                        <div class="bar">
-                            <div class="bar-inner animated-bar" data-percent="68%"><div class="count-text">68%</div></div>
-                        </div>
-                    </div>
-                    <div class="donate clearfix">
-                        <div class="donate float_left"><span>Goal: $78000 </span></div>
-                        <div class="donate float_right">Raised: $49000</div>
-                    </div>
-                    
-                </div>                        
-                        
-            </article>
-            <article class="item col-md-4 col-sm-6 col-xs-12">
-                <figure class="img-box">
-                    <img src="images/cause/8.jpg" alt="">
-                    <div class="overlay"><div class="inner-box"><div class="content-box"><button class="thm-btn style-2 donate-box-btn">donate now</button></div></div></div>
-                </figure>
-                
-                <div class="content">
-                    
-                    <div class="text center">
-                        <a href="#"><h4 class="title">Childrens to get their home</h4></a>
-                        <p>Etiam vitae leo diam pellentesque portaed eleifend ultricies risu, vel rutrum era commodos  Praesent finib congue euismod...</p>               
-                    </div>
-                    <div class="progress-box">
-                        <div class="bar">
-                            <div class="bar-inner animated-bar" data-percent="68%"><div class="count-text">68%</div></div>
-                        </div>
-                    </div>
-                    <div class="donate clearfix">
-                        <div class="donate float_left"><span>Goal: $78000 </span></div>
-                        <div class="donate float_right">Raised: $49000</div>
-                    </div>
-                    
-                </div>                        
-                        
-            </article>
-            <article class="item col-md-4 col-sm-6 col-xs-12">
-                <figure class="img-box">
-                    <img src="images/cause/9.jpg" alt="">
-                    <div class="overlay"><div class="inner-box"><div class="content-box"><button class="thm-btn style-2 donate-box-btn">donate now</button></div></div></div>
-                </figure>
-                
-                <div class="content">
-                    
-                    <div class="text center">
-                        <a href="#"><h4 class="title">We encourage girls education</h4></a>
-                        <p>Phasellus cursus nunc arcu, eget sollicitudin milacinia tempurs. Donec ligula turpis, egestas  at volutpat no liquam...</p>               
-                    </div>
-                    <div class="progress-box">
-                        <div class="bar">
-                            <div class="bar-inner animated-bar" data-percent="75%"><div class="count-text">75%</div></div>
-                        </div>
-                    </div>
-                    <div class="donate clearfix">
-                        <div class="donate float_left"><span>Goal: $92000 </span></div>
-                        <div class="donate float_right">Raised: $69000</div>
-                    </div>
-                    
-                </div>                        
-                        
-            </article>
-            
         </div>
         <ul class="page_pagination center">
             <li><a href="#" class="tran3s"><i class="fa fa-angle-left" aria-hidden="true"></i></a></li>
