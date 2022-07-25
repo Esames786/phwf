255db374ec5d3420761ccde6bd48f4d37dba674e
shariq <syedshariqali302@gmail.com>
2022-07-25 18:50:31 +0500

    changes

---
+++ publicMessage.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Public Help Welfare || Public Message</title><!-- mobile responsive meta -->
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <link href="images/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180">
+    <link href="images/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png">
+    <link href="images/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png">
+</head>
+
+<body>
+    <div class="boxed_wrapper">
+    <?php include './includes/header.php' ?>
+        <div class="inner-banner has-base-color-overlay text-center" style="background: url(images/background/4.jpg);">
+            <div class="container">
+                <div class="box">
+                    <h1>Public Message</h1>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="breadcumb-wrapper">
+            <div class="container">
+                <div class="pull-left">
+                    <ul class="list-inline link-list">
+                        <li>
+                            <a href="index.php">Home</a>
+                        </li>
+
+
+                        <li>
+                            <a href="#">Pages</a>
+                        </li>
+
+
+                        <li>Public Message</li>
+                    </ul>
+                </div>
+
+
+                <div class="pull-right">
+                    <a class="get-qoute" href="#"><i class="fa fa-arrow-circle-right"></i>Public Message</a>
+                </div>
+            </div>
+        </div>
+
+
+        <section class="about sec-padd2 style-2">
+            <div class="container">
+                <div class="row">
+                 
+
+
+                    <div class="col-md-8 col-sm-8 col-xs-12">
+                    <div class="img-box">
+                        <img src="images/event/16.jpg" alt="">
+                        
+                    </div>
+                        <div class="content">
+                        <div class="section-title2">
+                        <h3>A Public Message</h3>
+</div>
+<br>
+                            <div class="text">
+
+<p>We are all the human beings and have created for the purpose to help each other’s.  It is very famous proverbs that GOD HELPS THOSE WHO HELP THEMSELVES. Those who take decision of doing any good deed Almighty Allah helps them in a way that they cannot even imagine and beyond their thinking. We must all think only about the well-being of the society and solve the problems of society and God will care of ours.</p>                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+      
+
+
+    
+
+
+      
+
+
+
+        <?php include './includes/footer.php' ?>
+
+        <!-- Scroll Top  -->
+        <button class="scroll-top tran3s color2_bg"><span class="fa fa-angle-up"></span></button> <!-- preloader  -->
+
+
+        <div class="preloader">
+        </div>
+
+
+        <div class="donate-popup" id="donate-popup">
+            <div class="close-donate theme-btn">
+                <span class="fa fa-close"></span>
+            </div>
+
+
+            <div class="popup-inner">
+                <div class="container">
+                    <div class="donate-form-area">
+                        <div class="section-title center">
+                            <h2>Donation Information</h2>
+                        </div>
+
+
+                        <h4>How much would you like to donate:</h4>
+
+
+                        <form action="#" class="donate-form default-form">
+                            <ul class="chicklet-list clearfix">
+                                <li><input id="donate-amount-1" name="donate-amount" type="radio"> <label data-amount="1000" for="donate-amount-1">$1000</label></li>
+
+
+                                <li><input checked="checked" id="donate-amount-2" name="donate-amount" type="radio"> <label data-amount="2000" for="donate-amount-2">$2000</label></li>
+
+
+                                <li><input id="donate-amount-3" name="donate-amount" type="radio"> <label data-amount="3000" for="donate-amount-3">$3000</label></li>
+
+
+                                <li><input id="donate-amount-4" name="donate-amount" type="radio"> <label data-amount="4000" for="donate-amount-4">$4000</label></li>
+
+
+                                <li><input id="donate-amount-5" name="donate-amount" type="radio"> <label data-amount="5000" for="donate-amount-5">$5000</label></li>
+
+
+                                <li class="other-amount">
+                                    <div class="input-container" data-message="Every dollar you donate helps end hunger.">
+                                        <span>Or</span><input id="other-amount" placeholder="Other Amount" type="text">
+                                    </div>
+                                </li>
+                            </ul>
+
+
+                            <h3>Donor Information</h3>
+
+
+                            <div class="form-bg">
+                                <div class="row clearfix">
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <div class="form-group">
+                                            <p>Your Name*</p>
+                                            <input name="fname" placeholder="" type="text">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <div class="form-group">
+                                            <p>Email*</p>
+                                            <input name="fname" placeholder="" type="text">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <div class="form-group">
+                                            <p>Address*</p>
+                                            <input name="fname" placeholder="" type="text">
+                                        </div>
+                                    </div>
+
+
+                                    <div class="col-md-6 col-sm-6 col-xs-12">
+                                        <div class="form-group">
+                                            <p>Phn Num*</p>
+                                            <input name="fname" placeholder="" type="text">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <ul class="payment-option">
+                                <li>
+                                    <h4>Choose your payment method:</h4>
+                                </li>
+
+
+                                <li>
+                                    <div class="checkbox">
+                                        <label><input name="pay-us" type="checkbox"> <span>Paypal</span></label>
+                                    </div>
+                                </li>
+
+
+                                <li>
+                                    <div class="checkbox">
+                                        <label><input name="pay-us" type="checkbox"> <span>Offline Donation</span></label>
+                                    </div>
+                                </li>
+
+
+                                <li>
+                                    <div class="checkbox">
+                                        <label><input name="pay-us" type="checkbox"> <span>Credit Card</span></label>
+                                    </div>
+                                </li>
+
+
+                                <li>
+                                    <div class="checkbox">
+                                        <label><input name="pay-us" type="checkbox"> <span>Debit Card</span></label>
+                                    </div>
+                                </li>
+                            </ul>
+
+
+                            <div class="center">
+                                <button class="thm-btn" type="submit">Donate Now</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- jQuery -->
+        <script src="js/jquery.js">
+        </script> 
+        <script src="js/bootstrap.min.js">
+        </script> 
+        <script src="js/menu.js">
+        </script> 
+        <script src="js/owl.carousel.min.js">
+        </script> 
+        <script src="js/jquery.mixitup.min.js">
+        </script> 
+        <script src="js/jquery.fancybox.pack.js">
+        </script> 
+        <script src="js/imagezoom.js">
+        </script> 
+        <script src="js/jquery.magnific-popup.min.js">
+        </script> 
+        <script src="js/jquery.polyglot.language.switcher.js">
+        </script> 
+        <script src="js/SmoothScroll.js">
+        </script> 
+        <script src="js/jquery.appear.js">
+        </script> 
+        <script src="js/jquery.countTo.js">
+        </script> 
+        <script src="js/validation.js">
+        </script> 
+        <script src="js/wow.js">
+        </script> 
+        <script src="js/jquery.fitvids.js">
+        </script> 
+        <script src="js/nouislider.js">
+        </script> 
+        <script src="js/bootstrap-select.min.js">
+        </script> <!-- revolution slider js -->
+         
+        <script src="js/rev-slider/jquery.themepunch.tools.min.js">
+        </script> 
+        <script src="js/rev-slider/jquery.themepunch.revolution.min.js">
+        </script> 
+        <script src="js/rev-slider/revolution.extension.actions.min.js">
+        </script> 
+        <script src="js/rev-slider/revolution.extension.carousel.min.js">
+        </script> 
+        <script src="js/rev-slider/revolution.extension.kenburn.min.js">
+        </script> 
+        <script src="js/rev-slider/revolution.extension.layeranimation.min.js">
+        </script> 
+        <script src="js/rev-slider/revolution.extension.migration.min.js">
+        </script> 
+        <script src="js/rev-slider/revolution.extension.navigation.min.js">
+        </script> 
+        <script src="js/rev-slider/revolution.extension.parallax.min.js">
+        </script> 
+        <script src="js/rev-slider/revolution.extension.slideanims.min.js">
+        </script> 
+        <script src="js/rev-slider/revolution.extension.video.min.js">
+        </script> 
+        <script src="js/custom.js">
+        </script>
+    </div>
+</body>
+</html>
