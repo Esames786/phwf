296b15554fff9bd85a4f86132314e9ca9f37e1c4
shariq <syedshariqali302@gmail.com>
2022-07-25 16:25:44 +0500

    changes

---
+++ includes/footer.php
@@ -0,0 +1,170 @@
+<footer class="main-footer">
+            <!--Widgets Section-->
+
+
+            <div class="widgets-section">
+                <div class="container">
+                    <div class="row">
+                        <!--Big Column-->
+
+
+                        <article class="big-column col-md-6 col-sm-12 col-xs-12">
+                            <div class="row clearfix">
+                                <!--Footer Column-->
+
+
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <div class="footer-widget about-column">
+                                        <div class="section-title">
+                                            <h4>About Us</h4>
+                                        </div>
+
+
+                                        <div class="text">
+                                            <p>We partner with over 320 amazing projects worldwide, and have given over $150 million in cash and product grants to other groups since 2011. We also operate our own dynamic suite of Signature Programs, million in cash and product grants to others</p>
+                                        </div>
+
+
+                                        <div class="link">
+                                            <a class="default_link" href="#">Read More <i class="fa fa-long-arrow-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--Footer Column-->
+
+
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <div class="footer-widget link-column">
+                                        <div class="section-title">
+                                            <h4>Usefull Links</h4>
+                                        </div>
+
+
+                                        <div class="widget-content">
+                                            <ul class="list">
+                                                <li>
+                                                    <a href="about.html">About Our Humanity</a>
+                                                </li>
+
+
+                                                <li>
+                                                    <a href="cause-1.html">Recent Causes</a>
+                                                </li>
+
+
+                                                <li>
+                                                    <a href="volunteer.html">Our Volunteers</a>
+                                                </li>
+
+
+                                                <li>
+                                                    <a href="#">Our Donators</a>
+                                                </li>
+
+
+                                                <li>
+                                                    <a href="#">Sponsers</a>
+                                                </li>
+
+
+                                                <li>
+                                                    <a href="event-1.html">Upcoming Events</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                        <!--Big Column-->
+
+                        <article class="big-column col-md-6 col-sm-12 col-xs-12">
+                            <div class="row clearfix">
+                                <!--Footer Column-->
+
+
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <div class="footer-widget post-column">
+                                        <div class="section-title">
+                                            <h4>Recent Post</h4>
+                                        </div>
+
+
+                                        <div class="post-list">
+                                            <div class="post">
+                                                <a href="#">
+                                                <h5>Car show event photos 2015</h5></a>
+
+                                                <div class="post-info">
+                                                    March 14, 2017
+                                                </div>
+                                            </div>
+
+
+                                            <div class="post">
+                                                <a href="#">
+                                                <h5>Hope Kids Holiday Party</h5></a>
+
+                                                <div class="post-info">
+                                                    February 21, 2017
+                                                </div>
+                                            </div>
+
+
+                                            <div class="post">
+                                                <a href="#">
+                                                <h5>humanity Bikini Car wash photos</h5></a>
+
+                                                <div class="post-info">
+                                                    January 15, 2017
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--Footer Column-->
+
+
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <div class="footer-widget contact-column">
+                                        <div class="section-title">
+                                            <h4>Get In Touch</h4>
+                                        </div>
+
+
+                                        <ul class="contact-info">
+                                            <li><i class="icon-arrows"></i><span>Address:</span> Park Drive, Varick Str<br>
+                                            New York, NY 10012, USA</li>
+
+
+                                            <li><i class="icon-phone"></i> <span>Phone:</span> (123) 0200 12345 &<br>
+                                            1800-45-678-9012</li>
+
+
+                                            <li><i class="icon-back"></i><span>Email:</span> ailus@Humanity.com</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!--Footer Bottom-->
+
+        <section class="footer-bottom">
+            <div class="container">
+                <div class="pull-left copy-text">
+                    <p><a href="#">Copyrights © 2017</a> All Rights Reserved. Powered by <a href="#">Humanity</a></p>
+                </div>
+                <!-- /.pull-right -->
+
+
+                <div class="pull-right get-text">
+                    <a href="#">Join Us Now!</a>
+                </div>
+                <!-- /.pull-left -->
+            </div>
+            <!-- /.container -->
+        </section>
